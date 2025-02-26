83f1a686a173f355c748f1d37f1994d449443e9b
DontforgetAntaiku <vladodessa2012@gmail.com>
2025-02-27 00:19:11 +0100

    Fix

--- app/filters/filters.py
+++ app/filters/filters.py
@@ -1,13 +1,9 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
-from app.classes.singleton import SingletonFactory
-from app.config import load_config
+from app.config import Config
 
 
-class IsAdmin(SingletonFactory, BaseFilter):
-    def init(self, *args, **kwargs):
-        self.admins_id = load_config().tg_bot.admins_id
-
-    async def __call__(self, message: Message) -> bool:
-        return message.chat.id in self.admins_id
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message, config: Config) -> bool:
+        return message.chat.id in config
